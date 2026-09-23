84a839e87fec263f0b0ad1fc225ffe44106f466f
gamerpro3245 <161112188+gamerpro3245@users.noreply.github.com>
2026-09-23 23:42:44 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -46,8 +46,6 @@ class User(Base):
     teaching_assignments: Mapped[list["TeachingAssignment"]] = relationship(
         back_populates="teacher",
     )
-role: Mapped[str] = mapped_column(
-String(20),
 )
 
 
